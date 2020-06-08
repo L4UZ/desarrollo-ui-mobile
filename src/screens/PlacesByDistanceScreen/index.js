a179a4104c9d4e44c5235a6d4af5d802c8482cdd
Diego Lauz <diego.lauz@hotmail.com>
2020-06-08 18:42:00 -0300

    Places by distance placeholder

--- src/screens/PlacesByDistanceScreen/index.js
+++ src/screens/PlacesByDistanceScreen/index.js
@@ -1,48 +1,55 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import { Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Button, ActivityIndicator } from 'react-native-paper';
 import { shape, func } from 'prop-types';
+import { useLazyQuery } from '@apollo/react-hooks';
 import * as Location from 'expo-location';
 
+import { PLACES_BY_DISTANCE } from '../../api/queries';
 import styles from './styles';
 
 const PlacesByDistanceScreen = ({ navigation }) => {
-  const [location, setLocation] = useState(null);
+  const [getPlaces, { data, loading }] = useLazyQuery(PLACES_BY_DISTANCE);
 
   useFocusEffect(
     useCallback(() => {
       (async () => {
         const { status } = await Location.requestPermissionsAsync();
         if (status !== 'granted') navigation.navigate('Continents');
-        else if (!location) {
-          const { coords } = await Location.getCurrentPositionAsync({});
-          setLocation(coords);
+        else if (!data) {
+          const {
+            coords: { latitude, longitude },
+          } = await Location.getCurrentPositionAsync({});
+          getPlaces({ variables: { coords: { latitude, longitude } } });
         }
       })();
-    }, [location])
+    }, [data])
   );
+
+  if (!data || loading)
+    return (
+      <View>
+        <ActivityIndicator size="large" animating />
+      </View>
+    );
 
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        {location ? (
-          <>
-            <View style={styles.helpContainer}>
-              <Button
-                onPress={() => navigation.navigate('Place', { placeId: 'PLACE_ID_FROM_DISTANCE' })}
-              >
-                Go to place
-              </Button>
-            </View>
-            <Text>
-              `${location.latitude} ${location.longitude}`
-            </Text>
-          </>
-        ) : (
-          <ActivityIndicator size="large" animating />
-        )}
+        <>
+          <View style={styles.helpContainer}>
+            <Button
+              onPress={() => navigation.navigate('Place', { placeId: 'PLACE_ID_FROM_DISTANCE' })}
+            >
+              Go to place
+            </Button>
+          </View>
+          {data.placesByDistance.map(place => (
+            <Text key={place.id}>{`${place.name} (${place.distance})`}</Text>
+          ))}
+        </>
       </ScrollView>
     </View>
   );
