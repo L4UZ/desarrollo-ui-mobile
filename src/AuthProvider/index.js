a0ae8f0d49c74911b4bdcade78d8156b4d86f9e0
Diego Lauz <diego.lauz@hotmail.com>
2020-06-09 18:02:18 -0300

    Session persistence

--- src/AuthProvider/index.js
+++ src/AuthProvider/index.js
@@ -1,11 +1,27 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { AsyncStorage } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 const AuthContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 const Provider = ({ children }) => {
   const [token, setToken] = useState(null);
-  const value = { token, setToken };
+
+  useEffect(async () => {
+    const storedToken = await AsyncStorage.getItem('token');
+    if (storedToken) setToken(storedToken);
+    await SplashScreen.hideAsync();
+  }, []);
+
+  const setTokenOverride = newToken => {
+    setToken(newToken);
+
+    if (!newToken) AsyncStorage.removeItem('token');
+    else AsyncStorage.setItem('token', newToken);
+  };
+
+  const value = { token, setToken: setTokenOverride };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
